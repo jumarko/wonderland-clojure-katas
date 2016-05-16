6b9534a41f3773eeb3e6cbfab95ed05320cf7a0a
Juraj Martinka <juraj@snapshot.travel>
2016-05-16 13:31:31 +0200

    Living Clojure Week 4 - Implement encode function for alphabet_cipher kata

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,11 +1,35 @@
 (ns alphabet-cipher.coder)
 
+(defn- num-value [ch]
+  (- (int ch) (int \a)))
+
+(defn- char-value [num]
+  (char (+ num (int \a))))
+
+
+;;; Motivation:
+;;; a -> 0
+;;; b -> 1
+;;; ...
+;;; z -> 25
+;;; => mod 26
+
+(defn encode-char [keyword-char word-char]
+  (char-value
+   (mod  (+
+          (num-value keyword-char)
+          (num-value word-char)) 26)))
+
+
 (defn encode [keyword message]
-  "encodeme")
+  (let [message-size (count message)
+        keyword-chars (take message-size (cycle keyword))
+        encoded-chars (map encode-char keyword-chars  message)]
+    (apply str encoded-chars)
+    ))
 
 (defn decode [keyword message]
   "decodeme")
 
 (defn decipher [cipher message]
   "decypherme")
-
