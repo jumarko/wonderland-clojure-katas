340a919cc14c41cb522f01ec71f62614054e75a9
Juraj Martinka <juraj@snapshot.travel>
2016-06-22 20:51:26 +0200

    Living clojure Week 4 Day 2 - Implement decode function

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -14,22 +14,37 @@
 ;;; z -> 25
 ;;; => mod 26
 
-(defn encode-char [keyword-char word-char]
+(defn- translate-char [operation first-char second-char]
   (char-value
-   (mod  (+
-          (num-value keyword-char)
-          (num-value word-char)) 26)))
+   (mod  (operation
+          (num-value first-char)
+          (num-value second-char)) 26)))
 
+(defn- encode-char [keyword-char word-char]
+  (translate-char + keyword-char word-char))
 
-(defn encode [keyword message]
+(defn- decode-char [keyword-char encoded-char]
+  (translate-char - encoded-char keyword-char))
+
+(defn- decipher-char [encoded-char word-char]
+  (translate-char - encoded-char word-char))
+
+(defn- translate [translate-char-fn keyword message]
   (let [message-size (count message)
         keyword-chars (take message-size (cycle keyword))
-        encoded-chars (map encode-char keyword-chars  message)]
-    (apply str encoded-chars)
-    ))
+        translated-chars (map translate-char-fn keyword-chars  message)]
+    (apply str translated-chars)))
 
-(defn decode [keyword message]
-  "decodeme")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Public
+(defn encode [keyword message]
+  (translate encode-char keyword message))
+
+(defn decode [keyword encrypted-message]
+  (translate decode-char keyword encrypted-message))
 
 (defn decipher [cipher message]
-  "decypherme")
+  (let [full-length-keyword (translate decipher-char cipher message)]
+    ;; TODO we need to cut the reamining chars from full-length-keyword to get simple keyword
+    full-length-keyword))
