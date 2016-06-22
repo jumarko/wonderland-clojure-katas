c3a77c0b438a7a9a55f80c066abc9bed34c04021
Juraj Martinka <juraj@snapshot.travel>
2016-06-22 21:11:38 +0200

    Solution for Week 4 Day 3 problem - Wonderland number kata

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,17 @@
 (ns wonderland-number.finder)
 
+(defn- hasAllTheSameDigits? [n1 n2]
+  (let [s1 (set (str n1))
+        s2 (set (str n2))]
+    (= s1 s2)))
+
 (defn wonderland-number []
   ;; calculate me
-  42)
+  (first (filter #(and (hasAllTheSameDigits? % (* % 2))
+                       (hasAllTheSameDigits? % (* % 3))
+                       (hasAllTheSameDigits? % (* % 4))
+                       (hasAllTheSameDigits? % (* % 5))
+                       (hasAllTheSameDigits? % (* % 6))
+                       ) (range 100000 200000))))
+
+(wonderland-number)
