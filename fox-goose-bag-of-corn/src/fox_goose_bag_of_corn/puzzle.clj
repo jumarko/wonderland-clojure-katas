1bd6b6d3c8a6058b4724125bb8bba3b7f8865108
Juraj Martinka <juraj@snapshot.travel>
2016-06-24 11:21:02 +0200

    Solution for Week 4 Day 5 problem - Wonderland "Fox, Goose, and Bag of Corn" kata

--- fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
+++ fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
@@ -3,4 +3,25 @@
 (def start-pos [[[:fox :goose :corn :you] [:boat] []]])
 
 (defn river-crossing-plan []
-  start-pos)
+  "The idea of safe crossing plain is as follows:
+   - take goose to the other side and leave it there
+   - tak fox to the other side, leave it there, but take goose back to the first bank to make it safe
+   - take corn to the other side to make it safe and leave goose on the first bank
+   - finally go back and take goose once again to the other side -> all pieces are now on the other side of river"
+  [
+   [[:fox :goose :corn :you] [:boat] []]
+   [[:fox :corn] [:boat :you :goose] []]
+   [[:fox :corn] [:boat] [:you :goose]]
+   [[:fox :corn ] [:boat :you] [:goose]]
+   [[:fox :corn :you] [:boat] [:goose]]
+   [[:corn] [:boat :you :fox] [:goose]]
+   [[:corn] [:boat] [:goose :you :fox]]
+   [[:corn] [:boat :you :goose] [:fox]]
+   [[:corn :you :goose] [:boat] [:fox]]
+   [[:goose] [:boat :you :corn] [:fox]]
+   [[:goose] [:boat] [:fox :you :corn]]
+   [[:goose] [:boat :you] [:fox :corn]]
+   [[:goose :you] [:boat] [:fox :corn]]
+   [[] [:boat :you :goose] [:fox :corn]]
+   [[] [:boat] [:fox :corn :you :goose]]
+   ])
